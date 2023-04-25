49cb1d5f432b365ec36bdc1909704811261087e4
Brenner Paiva <114958953+brennerpaiva@users.noreply.github.com>
2023-04-25 11:12:17 -0300

    query params e rout params

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const express = require('express')
 
 const server = express();
 
+//Query params = ?nome=NodeJS
+//Route params = /curso/2
+//Request Body = { nome: 'Nodejs', tipo: 'Backend'}
+
+//Crud > Create, Read, Update, Delete
+
+
 //localhost:3000/curso
-server.get('/curso', (req, res) => {
-    return res.json({curso: 'Node JS'})
+
+const cursos = ['NodeJS', 'JavaScript', 'ReactNative'];
+
+server.get('/curso/:index', (req, res) => {
+
+    const { index } = req.params;
+
+    return res.json(cursos[index]);
 })
 
 server.listen(3000);
